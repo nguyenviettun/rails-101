2acc114d12ac39a12d1cc6caebfe658f9621fec7
nguyenviettun <tuan.nguyen@employmenthero.com>
2019-07-22 19:24:25 +0700

    Create user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,18 @@ class UsersController < ApplicationController
 
   def login; end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   # POST /users
   # POST /users.json
   def create
     @user = User.new(params[:user])
-    respond_to do |format|
-      if @user.save
-        # Tell the UserMailer to send a welcome email after save
-        UserMailer.with(user: @user).welcome_email.deliver_now
-        format.html { redirect_to(@user, notice: 'User was successfully created.') }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      
+    else
+      render create_path
     end
   end
 end
