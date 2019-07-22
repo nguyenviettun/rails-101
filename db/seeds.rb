a610503f819d7ec8e634b1766aa0db6225c2813f
nguyenviettun <tuan.nguyen@employmenthero.com>
2019-07-22 15:56:03 +0700

    Update seeds.rb
    Add a new user record.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,19 @@
 User.create!(
   name: 'Nguyen Viet Tuan',
+  email: 'example@railstutorial.org',
+  password: 'tuantungtoe',
+  password_confirmation: 'tuantungtoe'
+)
+
+User.create!(
+  name: 'Nguyen Viet Tuan',
   email: 'tuan.nguyen@employmenthero.com',
   password: 'tuantungtoe',
   password_confirmation: 'tuantungtoe'
 )
 
 User.create!(
-  name: 'Nguyen Tuan Minh',
-  email: 'minh.nguyen@employmenthero.com',
+  name: 'Nguyen V. Tuan',
+  email: 'tuan.nguyenviet271@gmail.com',
   password: 'minhman'
 )
