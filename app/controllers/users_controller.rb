293e4d68ecc4d2b471c8a055a7caeec219205725
nguyenviettun <tuan.nguyen@employmenthero.com>
2019-07-24 08:40:08 +0700

    Change method 'create' to 'new'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,26 @@
 
 # No description
 class UsersController < ApplicationController
-  def signup; end
-
-  def login; end
-
   def show
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      
     else
-      render create_path
+      render 'new'
     end
   end
 
   private
-    def user_params
-      params.required(:user).permit(:name, :email, :password, 
-                                    :password_confirmation)
-    end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#create'
+  get '/signup', to: 'users#new'
 
   resources :users
 end
